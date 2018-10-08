330598477a4f427ad29423cfb5a1027f0c335a7d
Vhansemann <43345753+Vhansemann@users.noreply.github.com>
2018-10-08 11:37:24 +0200

    Update events.js

--- aframe/examples/events.js
+++ aframe/examples/events.js
@@ -6,7 +6,12 @@ AFRAME.registerComponent('markerhandler', {
 
         animatedMarker.addEventListener('click', function(ev){
             if (animatedMarker.object3D.visible == true && ev.detail.cursorEl) {
-            window.open("https://www.w3schools.com"); 
+                const entity = document.querySelector('#animated-model');
+                const scale = entity.getAttribute('scale');
+                Object.keys(scale).forEach((key) => scale[key] = scale[key] + 1);
+                
+                // every click, we make our model grow in size
+                entity.setAttribute('scale', scale);
             }
         });
 }});
